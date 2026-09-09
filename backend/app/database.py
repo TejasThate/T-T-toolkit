be76fc8d73aa81ccdd422b90fc7ddbb673312c32
Tejas Thate <thatetejas1227@gmail.com>
2026-09-09 20:42:48 +0530

    Force IPv4 socket resolution to fix Network Unreachable error

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,7 +3,15 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from contextlib import asynccontextmanager
 
 import os
+import socket
 from dotenv import load_dotenv
+
+# Force IPv4 to prevent "Network is unreachable" errors with IPv6 DNS resolution (common on Render + Supabase/Neon)
+old_getaddrinfo = socket.getaddrinfo
+def new_getaddrinfo(*args, **kwargs):
+    responses = old_getaddrinfo(*args, **kwargs)
+    return [response for response in responses if response[0] == socket.AF_INET]
+socket.getaddrinfo = new_getaddrinfo
 
 load_dotenv()
 

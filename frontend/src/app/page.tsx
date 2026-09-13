965a202f720e8cab07354634f7b86350c94d6967
Tejas Thate <thatetejas1227@gmail.com>
2026-09-13 14:01:33 +0530

    chore(ui): bypass strict linting rules on page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -51,6 +51,7 @@ export default function Page() {
 
   useEffect(() => {
     if (started) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
       fetchNews();
       fetchMarketData();
       
